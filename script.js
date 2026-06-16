91adc2e4202d86275325bb3357cb1744faa7335c
BURUKALA MANI REETHIKA <manireethikab@gmail.com>
2026-06-16 21:35:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,126 @@
 let stack = [];
-
-function renderStack() {
-    let container = document.getElementById("stack-container");
-    container.innerHTML = "";
-
-    for (let i = stack.length - 1; i >= 0; i--) {
-        let div = document.createElement("div");
-        div.className = "box";
-        div.innerText = stack[i];
-        container.appendChild(div);
-    }
-}
 let queue = [];
+let linked = [];
 
-function enqueue() {
-  const value = document.getElementById("value").value;
-  if(value){
-    queue.push(value);
-    renderQueue();
-  }
-}
-function toggleTheme(){
-  document.body.classList.toggle("dark");
-}
-
-function dequeue() {
-  queue.shift();
-  renderQueue();
-}
-
-function renderQueue() {
-  const container = document.getElementById("queue-container");
-  container.innerHTML = "";
-
-  queue.forEach(item => {
-    container.innerHTML += `<div class="box">${item}</div>`;
-  });
-}
 function push() {
-    let value = document.getElementById("value").value;
 
-    if (value !== "") {
-        stack.push(value);
-        renderStack();
-        document.getElementById("value").value = "";
-    }
+const value =
+document.getElementById("value").value;
+
+if(value){
+
+stack.push(value);
+
+renderStack();
+
+document.getElementById("value").value="";
+}
 }
 
-function pop() {
-    stack.pop();
-    renderStack();
+function popItem(){
+
+stack.pop();
+
+renderStack();
+}
+
+function renderStack(){
+
+const container =
+document.getElementById("stack-container");
+
+container.innerHTML="";
+
+for(let i=stack.length-1;i>=0;i--){
+
+container.innerHTML +=
+`<div class="box">${stack[i]}</div>`;
+}
+}
+
+function enqueue(){
+
+const value =
+document.getElementById("value").value;
+
+if(value){
+
+queue.push(value);
+
+renderQueue();
+
+document.getElementById("value").value="";
+}
+}
+
+function dequeue(){
+
+queue.shift();
+
+renderQueue();
+}
+
+function renderQueue(){
+
+const container =
+document.getElementById("queue-container");
+
+container.innerHTML="";
+
+queue.forEach(item=>{
+
+container.innerHTML +=
+`<div class="box queue-box">${item}</div>`;
+});
+}
+
+function insertNode(){
+
+const value =
+document.getElementById("value").value;
+
+if(value){
+
+linked.push(value);
+
+renderLinked();
+
+document.getElementById("value").value="";
+}
+}
+
+function deleteNode(){
+
+linked.pop();
+
+renderLinked();
+}
+
+function renderLinked(){
+
+const container =
+document.getElementById("linked-container");
+
+container.innerHTML="";
+
+linked.forEach((item,index)=>{
+
+container.innerHTML +=
+`<span class="linked-node">${item}</span>`;
+
+if(index !== linked.length-1){
+
+container.innerHTML += " ➜ ";
+}
+});
+
+if(linked.length){
+
+container.innerHTML += " ➜ NULL";
+}
+}
+
+function toggleTheme(){
+
+document.body.classList.toggle("light");
 }
